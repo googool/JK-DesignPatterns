1f76702f39166ceba843d3385982c63a68f71bed
cjx <1067446576@qq.com>
2021-03-09 14:45:48 +0800

    test No14

---
+++ src/test/objectOriented/No14/ApiRequestTest.java
@@ -0,0 +1,15 @@
+package objectOriented.No14;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by chenjinxin on 2021/3/9 下午2:45
+ */
+class ApiRequestTest {
+
+    @Test
+    void createFromFullUrl() {
+    }
+}
