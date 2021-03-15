2bbb5b7fd75998d1a36c9e74d9bd799b92fc4a3f
cjx <1067446576@qq.com>
2021-03-15 15:11:04 +0800

    重构 generate() 函数

--- src/main/java/standardizationAndReconstruction/no34/case2/IdGenerator.java
+++ src/main/java/standardizationAndReconstruction/no34/case2/IdGenerator.java
@@ -1,8 +1,10 @@
 package standardizationAndReconstruction.no34.case2;
 
+import standardizationAndReconstruction.no37.IdGenerationFailureException;
+
 /**
  * Created by chenjinxin on 2021/3/15 上午10:46
  */
 public interface IdGenerator {
-    String generate();
+    String generate() throws IdGenerationFailureException;
 }
---
+++ src/main/java/standardizationAndReconstruction/no37/IdGenerationFailureException.java
@@ -0,0 +1,9 @@
+package standardizationAndReconstruction.no37;
+
+/**
+ * Created by chenjinxin on 2021/3/15 下午3:10
+ */
+public class IdGenerationFailureException extends Throwable {
+    public IdGenerationFailureException(String s) {
+    }
+}
