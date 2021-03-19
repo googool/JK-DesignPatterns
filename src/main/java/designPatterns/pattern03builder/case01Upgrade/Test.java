755215631390ce2dcece0ad5a260a9c1bb3dc2e6
cjx <1067446576@qq.com>
2021-03-19 16:53:38 +0800

    建造者模式

---
+++ src/main/java/designPatterns/pattern03builder/case01Upgrade/Test.java
@@ -0,0 +1,11 @@
+package designPatterns.pattern03builder.case01Upgrade;
+
+/**
+ * Created by chenjinxin on 2021/3/17 下午5:55
+ */
+public class Test {
+    public static void main(String[] args) {
+        ResourcePoolConfig name = new ResourcePoolConfig("name");
+        name.setMaxIdle(8);
+    }
+}
