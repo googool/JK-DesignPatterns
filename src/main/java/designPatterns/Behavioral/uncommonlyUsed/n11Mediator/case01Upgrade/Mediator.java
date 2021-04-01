6ea7e6ee8d2299dd625788dde4869205164e0269
cjx <1067446576@qq.com>
2021-04-01 11:08:03 +0800

    行为型：中介模式

---
+++ src/main/java/designPatterns/Behavioral/uncommonlyUsed/n11Mediator/case01Upgrade/Mediator.java
@@ -0,0 +1,13 @@
+package designPatterns.Behavioral.uncommonlyUsed.n11Mediator.case01Upgrade;
+
+/**
+ * Created by chenjinxin on 2021/4/1 上午11:00
+ */
+
+public interface Mediator {
+//    void handleEvent(Component component, String event);
+}
+
+
+
+
